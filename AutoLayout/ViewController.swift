a0ce9fe589f2e9878752335f85ece90903516be5
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-01 18:36:00 +0800

    Defining fixed height and width for a UIView object

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -24,13 +24,36 @@ class ViewController: UIViewController {
         self.view.addSubview(yellowView)
         
         // constraints
-        let viewsDictionary = ["top":yellowView,"bottom":redView]
+        let viewsDictionary = ["top":yellowView, "bottom":redView]
         
         //position constraints
-        let view_constraint_H:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[top]-10-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        let view_constraint_H2:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[bottom]-10-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewsDictionary)
-        let view_constraint_V:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[top(bottom)]-[bottom]-10-|", options: NSLayoutFormatOptions.AlignAllLeading, metrics: nil, views: viewsDictionary)
+        let view_constraint_FixedV: NSArray = NSLayoutConstraint.constraintsWithVisualFormat("V:[top(==200)]",
+            options: NSLayoutFormatOptions(0),
+            metrics: nil,
+            views: viewsDictionary)
+
+        let view_constraint_FixedH: NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:[top(==100)]",
+            options: NSLayoutFormatOptions(0),
+            metrics: nil,
+            views: viewsDictionary)
         
+        let view_constraint_H:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[top]-10-|",
+            options: NSLayoutFormatOptions(0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        let view_constraint_H2:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-10-[bottom]-10-|",
+            options: NSLayoutFormatOptions(0),
+            metrics: nil,
+            views: viewsDictionary)
+        
+        let view_constraint_V:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("V:|-20-[top(bottom)]-[bottom]-10-|",
+            options: NSLayoutFormatOptions.AlignAllLeading,
+            metrics: nil,
+            views: viewsDictionary)
+        
+        view.addConstraints(view_constraint_FixedV)  // Vertical at 200px (fixed height for yellowView)
+        view.addConstraints(view_constraint_FixedH)  // Horizontal at 100px (width height for yellowView)
         view.addConstraints(view_constraint_H)
         view.addConstraints(view_constraint_H2)
         view.addConstraints(view_constraint_V)
